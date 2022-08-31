93a77fba7a5ce933e75e1f1fc2681fb6fa1cc6a3
Brian Sayler <saylerb@users.noreply.github.com>
2022-08-30 18:55:42 -0600

    iteration: pull out magic string to const

--- iteration/main.go
+++ iteration/main.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated = repeated + character
 	}
 
