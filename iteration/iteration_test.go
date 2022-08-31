60e1c6b83c0dbf3ebecf77b2720d5157ca84b739
Brian Sayler <saylerb@users.noreply.github.com>
2022-08-30 19:11:25 -0600

    allow caller to supply number of repeats and add example to document

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,9 +1,12 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -11,8 +14,14 @@ func TestRepeat(t *testing.T) {
 	}
 }
 
+func ExampleRepeat() {
+	result := Repeat("a", 10)
+	fmt.Println(result)
+	// Output: aaaaaaaaaa
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
