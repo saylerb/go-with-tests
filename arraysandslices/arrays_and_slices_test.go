e71415f4a2fee60dedde136dd187fc39ef9aa149
Brian Sayler <saylerb@users.noreply.github.com>
2022-09-02 10:33:34 -0600

    example of assigning function to a variable to scope it to the test function

--- arraysandslices/arrays_and_slices_test.go
+++ arraysandslices/arrays_and_slices_test.go
@@ -7,10 +7,8 @@ import (
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
-
 	got := Sum(numbers)
 	want := 15
-
 	if got != want {
 		t.Errorf("got %d, want %d given, %v", got, want, numbers)
 	}
@@ -19,10 +17,20 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
-	checkSums(t, got, want)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
 
 func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		// Warning: reflect.DeepEqual is not type safe
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	}
+
 	t.Run("sum the tails of two slices with 2 elements", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
@@ -34,12 +42,4 @@ func TestSumAllTails(t *testing.T) {
 		want := []int{0, 9}
 		checkSums(t, got, want)
 	})
-}
-
-func checkSums(t testing.TB, got, want []int) {
-	t.Helper()
-	// Warning: reflect.DeepEqual is not type safe
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
 }
