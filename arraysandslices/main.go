45c1dbfeab350a4207cdaa202f0b53498de815f5
Brian Sayler <saylerb@users.noreply.github.com>
2022-09-02 09:41:43 -0600

    sum function can take a slice of any length instead of fix-length array

--- arraysandslices/main.go
+++ arraysandslices/main.go
@@ -2,7 +2,7 @@ package arraysandslices
 
 import "fmt"
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for index, number := range numbers {
 		fmt.Println("index", index, "number", number)
