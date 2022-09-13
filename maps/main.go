7af6516d12d0646656fd7805820cd8fec3cfc516
Brian Sayler <saylerb@users.noreply.github.com>
2022-09-13 17:59:46 -0600

    error handling for when the word does not exist

--- maps/main.go
+++ maps/main.go
@@ -1,7 +1,15 @@
 package main
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+import "errors"
+
+func (d Dictionary) Search(key string) (string, error) {
+	result := d[key]
+
+	if result == "" {
+		return result, errors.New("could not find the word you're looking for")
+	}
+
+	return d[key], nil
 }
 
 type Dictionary map[string]string
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,12 +3,27 @@ package main
 import "testing"
 
 func TestMap(t *testing.T) {
-	dictionary := Dictionary{"test key": "test value"}
+	t.Run("known key", func(t *testing.T) {
+		dictionary := Dictionary{"test key": "test value"}
+		got, _ := dictionary.Search("test key")
+		want := "test value"
 
-	got := dictionary.Search("test key")
-	want := "test value"
+		if got != want {
+			t.Errorf("got %q, want %q, given %q", got, want, "test key")
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %q, want %q, given %q", got, want, "test key")
-	}
+	t.Run("unknown key", func(t *testing.T) {
+		dictionary := Dictionary{"test key": "test value"}
+		_, err := dictionary.Search("unknown key")
+		want := "could not find the word you're looking for"
+
+		if err == nil {
+			t.Fatal("we expected an error to be returned")
+		}
+
+		if err.Error() != want {
+			t.Errorf("got %q, want %q, given %q", err, want, "unknown key")
+		}
+	})
 }
