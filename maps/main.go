d57f3a4971a88fced9c2711897543c5fc31c6ecd
Brian Sayler <saylerb@users.noreply.github.com>
2022-09-13 18:25:42 -0600

    we can create add method to add a new word to the map

--- maps/main.go
+++ maps/main.go
@@ -4,14 +4,20 @@ import (
 	"errors"
 )
 
+var ErrNotFound = errors.New("could not find the word you're looking for")
+
+type Dictionary map[string]string
+
 func (d Dictionary) Search(key string) (string, error) {
 	result, ok := d[key]
 
 	if !ok {
-		return result, errors.New("could not find the word you're looking for")
+		return result, ErrNotFound
 	}
 
 	return d[key], nil
 }
 
-type Dictionary map[string]string
+func (d Dictionary) Add(key string, value string) {
+	d[key] = value
+}
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestMap(t *testing.T) {
+func TestSearchDictionary(t *testing.T) {
 	t.Run("known key", func(t *testing.T) {
 		dictionary := Dictionary{"test key": "test value"}
 		got, _ := dictionary.Search("test key")
@@ -16,14 +16,29 @@ func TestMap(t *testing.T) {
 	t.Run("unknown key", func(t *testing.T) {
 		dictionary := Dictionary{"test key": "test value"}
 		_, err := dictionary.Search("unknown key")
-		want := "could not find the word you're looking for"
 
 		if err == nil {
 			t.Fatal("we expected an error to be returned")
 		}
 
-		if err.Error() != want {
+		want := ErrNotFound
+		if err != want {
 			t.Errorf("got %q, want %q, given %q", err, want, "unknown key")
+		}
+	})
+}
+
+func TestAddToDictionary(t *testing.T) {
+	t.Run("add a key to the dictionary", func(t *testing.T) {
+		dictionary := Dictionary{}
+
+		dictionary.Add("test key", "test value")
+
+		got, _ := dictionary.Search("test key")
+		want := "test value"
+
+		if got != want {
+			t.Errorf("got %q, want %q, given %q", got, want, "test key")
 		}
 	})
 }
